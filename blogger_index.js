b895663f8f30c7092b38ad170bdbf9bee49b0ba5
Ajay Verma <ajayverma@Ajays-Air.fritz.box>
2018-06-16 19:48:02 +0530

    first commit

---
+++ blogger_index.js
@@ -0,0 +1,99 @@
+var serviceDetailArr = {
+    floor_plans: {
+        name: "Floor Plans",
+        desc: "Floor plans description",
+        img_src: "https://lh3.googleusercontent.com/TeaJizBQTjANqM6UB3-Kp32DMVU-MLt98mHRGlJmvlpMnXAMK_GKnwaIHKGsWoCpz6sPwcKRu4PGC0j_iYiGqaBZHTPVAB2E0Zn9AMpme48afMAAyX7NYZI3Z7OEP_TaOmLm9MPbXAWcxxy5oJ4WmSmY5st_1CbBpaC9cTnDfHsaBbQcz-QZIw4SR9pIY17ltGJfAgIpsoVuBe9KeKzlwgMqrCukgzxB-4lcq3XtHALRG4nyNbaiSmvxTNgiokukzf7Fdf_0cIbPJSVoUKrgHttE3eotpbrYBjAzSLzxMsRxQp89-UYuS8LtsFxEBuPA38pb0-RLsh43aybTHvume5trqNB8Ipiz9-RuT2nGiQVllOQJezo7OhqkXbxpIjYan9g0YJ8C3CMRK5gB6rznolchHjr6j5gZaZQG6a3n6vzBbEXb3ruDwmn1Qg8aZ6yJZL-t5x00_8xvYkVXE00WdHXgTceyE9Q22Oq3FnPpZN6dCoqDb0P4GiKXaQcVL0r4XSWombKntlhIE50UZt93dduj9DtsOcKkPDP9mIXykFobIlIU0lpxSfhiYZ7xYimy_OV7NsIQJ28YOIaXzDZnbyOLwx60BdsQFRP-sqU=w600-h450-no"
+    },
+    elevations: {
+        name: "Elevations",
+        desc: "Elevations description",
+        img_src: "https://lh3.googleusercontent.com/qLmW3jx94tVK0cV7YvGYP4NHIUeg5ezCh2X7kth1K1MBMmOwZmaFrdDIev9-vAaO9AyEpDUQqVkhHZxhPEchc0ZomfBFYsHm-DsgWvWAVCVPMxPPBjLSfFDt0YwE8mITyxpVvhT4InmVbB5dLjrz6nvjs9RzBkBVAKBovXEZsGe_hCObWOG4RVoOmrraUBsuOsGA7w9KacOxkand09srO5XC4-gnK8VGOhFOMLuCaoMbtatGrd4_XXniM256xY7lTHG-qCl8dkRO_6f5_DT1Ys_25H2QN_Pmvb2vjNJVVP6CkJI0c8lk4zi-lkUeEhGXGj8p1anfkHElHE-M3b9UelVoDKBwJu3PAZNRTfhCKH9B9P9ezibifGY8tfl00moX7qqZB5_dGdF5xDWyEyLwuyJVUaQ5GIdPL_iJ68ifcqFvRTw7mBGQxM79SWLl6A6UX_yycfZW6IuCA-xIVYx9THhPn7R4FoXis9RBo4FkGCfWdCXP6AbEsj3hc6PgZvx9kc4Nt-YMGfGNFgI9UWyOiqjsDXohgDyGNx3JeUQAxYEIsxwp8myN-CH9EwcWuXbGj15QfJsdS9c-CoAs-PA3jEns8ql0lJ6EN6hOPPY=w898-h694-no"
+    },
+    sections: {
+        name: "Sections",
+        desc: "Sections description",
+        img_src: "https://lh3.googleusercontent.com/B_SzzcCJXZFm8Uv9Qnf-U2y1QoXojFNh4A_VRhxirmDxYWbIVtjtq_f27f6hdaqqTKNdyJUMoflJjEfs9XqpEwIN7cV0UkwkC_mehTjvRvklPs_nWmtLtkJNYpANM1t5PzCDhDQrOHM9Yy71HYI6uUQ_8m6c_6tYllOMLNWuaPNjJD5_I4vYhvjuMQM4jZbvGH1e8MCkE8JYSg34aaRo52TAc0Zbe7aXAKSNLWk7tSFJ4WsqgVFYBeaxI3U42eEKjxe5Fos-4xGigRUY3fsSyOHG-B1X21e8r4u6ddHB7eHMBYAGqt-snQYtAzjncDMeYxmnWscJgMBiv2jWVtAhLKBxB0okO4QfIDeXEj6HFrAythRi7OGw4nvh6p3LntFPDr-QodkIRVKLDkSB-RGrOHVLj1FDcCkulepiiaR5j009dSph-kAJJFl0YE_482YGx_wybZ8SJqt58v8UQ3iAjthcFwx8ZvdH66g0K01kDPPm0E6oQ9TQW55rn5w_qcfXSVVoGI3wK_DXUWMAkXuHR2ljNnuvejVyx0_aaJU6g7eFGvT-wVxMaTzcjV_N102OxzXtEmDdpuZCwRPSua2kJNlS8XrdSZhKWliVv1U=s694-no"
+    },
+    roof_plans: {
+        name: "Roof plans",
+        desc: "Roof plans description",
+        img_src: "https://lh3.googleusercontent.com/mKJH20lS0sJh5XNBpcc3Bc_KxUXVaQtMpaRIQ6jfRKWXQ8n9i9OHba4Ko98Hklw4K1KNNFWJztRgQICPh-VKQu-21NMy7yXX5IjkWz01ecF_4MnzAFyf0kHAJScwSnKPLiHwYdCbUFrgzFQLr7FnD91waEfiPoRfoIu0KNUkOUrdrDq_6f26QrP0YSIn2o9s2D56zkX0A2H6zmDOySimDPYJsHpRRHCWN4i8DuqFLOq7VxgCV53a7pVKcfNlehMQgmFs9yoaCBCdT6aWBR27KaWMBrtDwugXVH9VKBfyF0RP8Jtzp3GbF_prYL6kDZXrH9Qtj422V25sgmt4KGShzL-bRk_Yky_WDM9Q1S1UOZzlU7p8T8TStdAW76x8CrFdsD8NryXrKKRyq5MgjPg8Cl5WMyzPT8xe9f-1pfWxqGbW8IyJyzVvsOlW-ZxBYYKNZKqiPz7gQ-S-p0lakSk7qc5QSPwuy4-lSx9Mu8VyvP9eH4eYVwhzqX-UC4RPtIkCJLqJIMKnV2674MO6WcYT-yOrpptl0Ps2JlibA1lX9xRS-YRL4itkwl7vba6jw3o9HC_xWkDFbZhmumbeXW2tW7V9aH3AJLGKxkEPaPM=w524-h454-no"
+    },
+    electrical_plans: {
+        name: "Electrical plans",
+        desc: "Electrical plans description",
+        img_src: "https://lh3.googleusercontent.com/6Q0J7CRhici7cNoXhaGcJbzHD5cFcEKhI9PKO7eovQQyK1ddpxafkWBNe36v24jUrcn-ZnBj-Z3k2Ln7hTdBISLK43cBTMRciGgBawREODM5LgYuqdxg84JLFeJjHYy1NhL-T6CJPj1G-T7Z2lkSP-JMG8CaieRvyNmnYkEgGn79zLQCDatGo3Mj6l0tEFBa-X4zShNBwZYE2BW2LvXM-t36XQeqrs-ROlhKSb31r6Oy23CDTiDMtbOhodlxVAYUSiXprM9ES0ErhuS2kz3jJc1bzfnGcgHxW68bVCuHpLrXUhs8f9M7jHVzdZChKqBNkNTqWQ5NnpLb0_wWNSQgtA44aASqyOnJzQRmU-OhxP-SzlfUA2hEY22ZwqjNvib9SFPs9VOx1ne_bjJBYR4dGI7fU8H_HSR8_5cill4hfkDSICxHvt-3cOw-67sOYg0tzqvY5fibGzejITpg1e-slji50Oedn2o9w-vDUunQc7kpPR_iXdeEx8_ToPuhhpvazqOecKn--eDenUZo-vJBDuqJUJko4M_AWQmRwfjRH7tYVm2dKCcnYA7bzI3ylYdyTJBXtP6Qy59URq4H2Oz6ppuI9WOeJaFIZdphoTI=w600-h424-no"
+    },
+    plumbing_plans: {
+        name: "Plumbing Plans",
+        desc: "Plumbing Plans description",
+        img_src: "https://lh3.googleusercontent.com/lGL9y3CZYgZP6kTrtaM1RTi5pXs4-AjvdbMq2bHm4vcpajimIyi4n39C8GzFsGpW_XPlapCv_vSSlYQ4FBniW95K3ayOGkgpRwlLaQ87QjfUhqwgrnUuURYfr5PyL-zfiWmP-t9CnGNZrmbRPzpUJHh_8KzrCbxVcSqyrgQL4I3Utli0g4-an9XzpNff0Dx0k3OoesQl8jHydSvfvQXbzfCiEDpa2-L44wNHfYhWrZXA4fgNBlTTyuNCscsq2sVgosyku_W0OswFda6akVp3qCS0hewX94gjJ18RspWeg9q4WJq_4XrfJ0n7MLfItW7nc9y_HAoqldtUtYDOUDBi2IYJVjLZVWsmvVIPYIJBBdckkAcWrYyvunPi5LtLyaFfSYpLgNrF7-CIPlj0ffHV5n81jfSNGoQ_eRh8X79ZxBlecq_vwjwD3yj1z6zPaxE0oqlW28YGr_DvoQX5wmVzPbp2JtERLmURxlmWKkad7LLSWqB-z8pQGoqDc-_zCWcMCXTrLe6gF4JQPAd7vioRS0rhkHcAVoZcsUPKGQQk-hL2eL2fWyXlYbSCBiN-lMwd5bKKMygK4kQv8LccR1tXoC0W_j6-pik6Pk0TCKY=w300-h264-no"
+    },
+    shop_drawings: {
+        name: "Shop Drawings",
+        desc: "Shop Drawings description",
+        img_src: "https://lh3.googleusercontent.com/TZh58GyNmGogCBZygjjBAU5T_ORqIOh3bgv35HCUEFTkDV5qSDTRJ0OEgD1ATVNhafTl6v0PSuCCN8Up6lUx4v3M2vm8fBBJ85rNDBgaoVeF_hfxn_YRbB7lJEU7d-wrEXt5gtTb02egq3FWKA9fmoC-322E_nLkKCLaBe6QCMEWC0GUVPLSrE4WRNX8mfuYtghGwLdZ7TJQAeEyQK7TSxmo2i-RGdKv2st40MSlKcUxTu6Ud5QpV70QvfTs_RT7SZgIlLL0ax9iLFzHOkVBqICgpheNznwkdrEA9tk-MNrJ1yjrStKkmn18xIiWFRyi6dA74wlpuXbRLdY1cbSTK3S13_cFMd7sKQSackZY6gD_jMlAA56KXh7R4ErPeAb0qtow5GEUKLClyhyopaH3VsDM2vvdA4_RfV87dt3hs6udhlhfay6x2ldZNo2kjLX30z6vjBf2HPYtRlFecWWiUgZCQbrh8cmbPUO5tf2RvP1u3aaLTfHNFvWvzyWasEgpY4uOZUb3yUB5FmmRr8jmUWoDNE4iF9C_p3V77diSYo3XrNsrLY56HVJgfwqaz53bgbuG-pQIdPKo3uwAL1FSMN170pc5qPYMSzTWkIA=w1023-h694-no"
+    },
+    wardrobe_drawings: {
+        name: "Wardrobe Drawings",
+        desc: "Wardrobe Drawings description",
+        img_src: "https://lh3.googleusercontent.com/DL1viTL0PX6BrBwTnA2SYXfviH9s3sOWWgcALTJQ1HFxBqLG0mTpeC8l-friF-AjhF6nx9glPRcFaIh0pdqTlia-bp93NTB8zXur54VF6WAkvBK_adFJcWFx-zV2LidKgBtYoI_47zGVjAY-mf0s6jvsZAYSX6EW4yp_pROeYelH3Ho0ocw2jMYaDqYnidfdE1hIgyDr88aq41XR3bE_YhnjN1-C-cFrlDz75ZIhqVrUvuGuITBcva5fNa164CHed8YZDueGpp3IAPWlZy_GL5STQQXi77xMGkLUjwxeR3LNcFa1QjyPCtq9FDrl2vJoOeRoKwyIa1hVoc8jStDsHoYJllg5c4pr31bC6yr5AL3094PJYr6b35TKPaQUvYaAbFUcVUb5v5VpIBIWAPkuO8kreqi7Tk29aKoYsnK4rkL3wCQnfIOxshAzme5LZ6j5ODWle9orDX2aEVy_ZcBglbxwEFwSs0mPMLwrZMxgQXICRf_YP5JgNIrAaXtyW7cfq5e58GgsQD6HfBI8Zwn29yOCFww-R5MrmTXetmKuxY-ATkixVmAHwd1NlGagGD1C7POTnL49G6wZGwaFT0TVvA_wQdot7d3_AsjN1dU=w796-h418-no"
+    },
+    laser_cut_drawings: {
+        name: "Laser Cut Drawings",
+        desc: "Laser Cut Drawings description",
+        img_src: "https://lh3.googleusercontent.com/VmcBZ5M8LFs2yvRQvRNsn8iaIJPRlJaIdzMy1BdguCriP1MuntX-JvzK30TwKsWdn3Lwq1iWt-3Lgqa3MM1cZsdshQ9h4Opg0nWMeoGBXnPlT0rB_QqSACKAKcacR1J29RNy2OL0JaNmvHVhNkrOJvC0_jvrsbsSE0uuaDzG58J-qLdle21mwjPLF9cc9-FI7GeNf5KkmDzV-ZsCEINM3OUB66_TP_q85RKkHGP-fzC1IxaD-NNEG2lyI_KJ22iYyuaFREbhO6vHrzqw0kzfT8QdtjEPODUsd6seSdROxffmtDxqeztqxXxTP1SFGOTcVSb2zKQKv09Xiq9IkzcvvR4pJeK2CxpByVawxhRKWLYPkSyvJzVhE6Ly2hH1m4EoqjNRsGrqLdcHuQhW1DoauFD7XKBd_RsA3ftL7n_WUXJwo4V0JKo_GVg_edfNqItjCFXZikVzT5YY4-4sw9cjtL3OadJ6W-xoPuxqAKMZUQyH_JIq8noneRKt1Yx05ZhqUotqJ4-pZ5g3Ks5dXqemVR0pMGbtTB0y-OWoC2BLpzXHrdAE_SJ0s535sMwnZJlorZ70hRYAfP8o00D0M3EK-uAZpggswUO33AyN54c=w628-h472-no"
+    }
+}
+
+
+
+$(document).ready(function () {
+
+    jQuery('.service-item').click(function () {
+        var serviceType = $(this).attr('name');
+        var selectedServiceTypeDetail = serviceDetailArr[serviceType];
+        $('.img_append img').attr('src', selectedServiceTypeDetail.img_src);
+        $('.desc_append').html(selectedServiceTypeDetail.desc);
+
+        $('.service-item').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    var native_width = 0;
+    var native_height = 0;
+    $(".large").css("background", "url('" + $(".small").attr("src") + "') no-repeat");
+
+    //Now the mousemove function
+    $(".magnify").mousemove(function (e) {
+        if (!native_width && !native_height) {
+            var image_object = new Image();
+            image_object.src = $(".small").attr("src");
+            native_width = image_object.width;
+            native_height = image_object.height;
+        } else {
+            var magnify_offset = $(this).offset();
+            var mx = e.pageX - magnify_offset.left;
+            var my = e.pageY - magnify_offset.top;
+
+            if (mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0) {
+                $(".large").fadeIn(100);
+            } else {
+                $(".large").fadeOut(100);
+            }
+            if ($(".large").is(":visible")) {
+                var rx = Math.round(mx / $(".small").width() * native_width - $(".large").width() / 2) * -1;
+                var ry = Math.round(my / $(".small").height() * native_height - $(".large").height() / 2) * -1;
+                var bgp = rx + "px " + ry + "px";
+
+                var px = mx - $(".large").width() / 2;
+                var py = my - $(".large").height() / 2;
+                $(".large").css({
+                    left: px,
+                    top: py,
+                    backgroundPosition: bgp
+                });
+            }
+        }
+    });
+});
